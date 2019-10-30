1760fad22785fba1753a2879f42e45ae9e7d41a5
Paul Wang <paul_wang347@hotmail.com>
2019-10-30 01:23:35 +0000

    Exec for nohup

--- exec.sh
+++ exec.sh
@@ -1,6 +1,10 @@
+#!/bin/bash
+
 export NUM_WORKERS=${1:-3}
 export ID_SERVER=${2:-}
 export DEFAULT_SERVER=${DEFAULT_SERVER:-http://104.42.36.58:5000}
+export USE_DEFAULT_SERVER=${USE_DEFAULT_SERVER:-1}
+
 chmod +x "$(pwd)/miner_impls/bin/go_impl"
 
 python3 miner-control.py --numWorkers $NUM_WORKERS $([[ -n "$ID_SERVER" ]] && echo "--idServer $ID_SERVER") $([[ -n "$USE_DEFAULT_SERVER" ]] && echo "--idServer $DEFAULT_SERVER") "$(pwd)/miner_impls/bin/go_impl"
